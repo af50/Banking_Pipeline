7669237e0120842c3e6f6f2a2e59b281bc027ea9
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-22 04:49:47 +0300

    Update stg_card_transactions.sql

---
+++ banking_dbt/models/staging/stg_card_transactions.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select *
+    from {{ source('silver', 'transactions_data') }}
+    where upper(channel) = 'CARD'
+)
+
+select
+    transaction_id,
+    customer_id,
+    card_id,
+    amount_mad as amount,
+    currency,
+    transaction_date,
+    transaction_time,
+    city,
+    merchant_category,
+    channel,
+    is_fraudulent,
+    _silver_processed_at
+from source
