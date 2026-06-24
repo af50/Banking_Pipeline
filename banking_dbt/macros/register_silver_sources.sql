19b91faec1805c3e8a0bf5637a4e7f9319cd6214
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-25 00:59:47 +0300

    Add macro to register Silver sources as DuckDB views
    This macro registers multiple Silver Delta/Parquet files as DuckDB views, allowing for easier access during dbt runs.

--- banking_dbt/macros/register_silver_sources.sql
+++ banking_dbt/macros/register_silver_sources.sql
@@ -1,1 +1,30 @@
 
+-- macros/register_silver_sources.sql
+-- Registers Silver Delta/Parquet files as DuckDB views
+-- Called once before dbt run via on-run-start hook
+
+{% macro register_silver_sources() %}
+
+  {% set silver_base = 'D:/NTI INTERNSHIP/Airflow/Banking_pipeline/local_warehouse/delta/silver' %}
+
+  {% set tables = [
+    'atm_master',
+    'customers',
+    'cards',
+    'card_transactions',
+    'wallet_transactions',
+    'out_of_cash',
+    'kaggle_transactions',
+    'pan_customer_map',
+  ] %}
+
+  {% for table in tables %}
+    {% set path = silver_base ~ '/' ~ table ~ '/**/*.parquet' %}
+    {% call statement('register_' ~ table) %}
+      CREATE OR REPLACE VIEW {{ schema }}.{{ table }} AS
+      SELECT * FROM read_parquet('{{ path }}', hive_partitioning=true);
+    {% endcall %}
+    {{ log("Registered silver view: " ~ table, info=True) }}
+  {% endfor %}
+
+{% endmacro %}
