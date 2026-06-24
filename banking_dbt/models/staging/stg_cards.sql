ffccefa4bcb7dfd40635da08bd04b6aaf81ebca3
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-24 21:39:23 +0300

    Remove stg_cards.sql staging view definition

--- banking_dbt/models/staging/stg_cards.sql
+++ banking_dbt/models/staging/stg_cards.sql
@@ -1,17 +1,1 @@
-{{ config(materialized='view') }}
 
-with source as (
-    select *
-    from {{ source('silver', 'cards_data') }}
-)
-
-select
-    card_id,
-    customer_id,
-    card_type,
-    card_status,
-    credit_limit_mad as credit_limit,
-    issue_date,
-    expiry_date,
-    _silver_processed_at
-from source
