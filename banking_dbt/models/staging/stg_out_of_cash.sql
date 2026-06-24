dc4c577845a0ab8adffd7f7694fd395b8c1ff6cf
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-24 21:37:28 +0300

    Update stg_out_of_cash.sql

--- banking_dbt/models/staging/stg_out_of_cash.sql
+++ banking_dbt/models/staging/stg_out_of_cash.sql
@@ -1,15 +1,1 @@
-{{ config(materialized='view') }}
 
-with source as (
-    select *
-    from {{ source('silver', 'out_of_cash') }}
-)
-
-select
-    atm_id,
-    event_date,
-    city,
-    governorate,
-    status,
-    _silver_processed_at
-from source
