6dc476ed80cd455fd0af872956e505676f76e7eb
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-24 21:36:51 +0300

    Update stg_wallet_transactions.sql

--- banking_dbt/models/staging/stg_wallet_transactions.sql
+++ banking_dbt/models/staging/stg_wallet_transactions.sql
@@ -1,16 +1,1 @@
-{{ config(materialized='view') }}
 
-with source as (
-    select *
-    from {{ source('silver', 'wallet') }}
-)
-
-select
-    wallet_id,
-    customer_id,
-    amount_mad as amount,
-    transaction_date,
-    city,
-    channel,
-    _silver_processed_at
-from source
