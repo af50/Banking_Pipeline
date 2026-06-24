3b1fa53727c0cc72c0b5c43d5e829c11ede6c8eb
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-25 01:01:23 +0300

    Create staging view for ATM transactions

--- banking_dbt/models/staging/stg_atm_transactions.sql
+++ banking_dbt/models/staging/stg_atm_transactions.sql
@@ -1,1 +1,32 @@
 
+-- staging/stg_atm_transactions.sql
+
+{{ config(materialized='view', tags=['staging']) }}
+
+WITH raw AS (
+    SELECT * FROM read_parquet(
+        'D:/NTI INTERNSHIP/Airflow/Banking_pipeline/local_warehouse/delta/silver/card_transactions/**/*.parquet',
+        hive_partitioning = true
+    )
+)
+
+SELECT
+    refnum                              AS refnum,
+    termid                              AS atm_id,
+    CAST(amount_mad AS DOUBLE)          AS amount_mad,
+    CAST(transaction_date AS DATE)      AS transaction_date,
+    CAST(transaction_hour AS INTEGER)   AS transaction_hour,
+    transaction_type                    AS transaction_type,
+    CAST(msg_type AS INTEGER)           AS msg_type,
+    CAST(resp_code AS INTEGER)          AS resp_code,
+    CAST(is_successful AS BOOLEAN)      AS is_successful,
+    CAST(is_reversal AS BOOLEAN)        AS is_reversal,
+    CAST(is_out_of_cash AS BOOLEAN)     AS is_out_of_cash,
+    CAST(is_deposit AS BOOLEAN)         AS is_deposit,
+    CAST(client_id AS VARCHAR)          AS client_id,
+    channel                             AS channel,
+    currency                            AS currency,
+    _silver_loaded_at
+FROM raw
+WHERE refnum IS NOT NULL
+  AND CAST(amount_mad AS DOUBLE) >= 0
