7c4d2338f7268604b32e5f8e27c12cd0bb62246a
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-24 21:38:40 +0300

    Delete stg_atm_master.sql view definition
    Removed the SQL view definition for stg_atm_master.

--- banking_dbt/models/staging/stg_atm_master.sql
+++ banking_dbt/models/staging/stg_atm_master.sql
@@ -1,16 +1,1 @@
-{{ config(materialized='view') }}
 
-with source as (
-    select *
-    from {{ source('silver', 'atm_master') }}
-)
-
-select
-    atm_id,
-    region,
-    governorate,
-    city,
-    daily_limit_mad as daily_limit,
-    status,
-    _silver_processed_at
-from source
