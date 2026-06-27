fcc72fdca750254d8043403575863cc2fa56a362
Mahmoud  Saad <89339019+Mahmoud2saad@users.noreply.github.com>
2026-06-27 22:23:43 +0300

    Refactor stg_atm_master.sql to use source function
    Updated SQL view definition to source data from a defined source instead of a file path and added row qualification.

--- banking_dbt/models/staging/stg_atm_master.sql
+++ banking_dbt/models/staging/stg_atm_master.sql
@@ -1,30 +1,28 @@
-
 {{ config(materialized='view', tags=['staging']) }}
 
 WITH raw AS (
-    SELECT * FROM read_parquet(
-        'D:/NTI INTERNSHIP/Airflow/Banking_pipeline/local_warehouse/delta/silver/atm_master/**/*.parquet',
-        hive_partitioning = true
-    )
+    SELECT * FROM {{ source('silver', 'atm_master') }}
 ),
 
 cleaned AS (
     SELECT
-        terminal_id                             AS atm_id,
-        region                                  AS region,
-        atm_type                                AS atm_type,
-        provider                                AS provider,
-        location_type                           AS location_type,
-        installation_date                       AS installation_date,
-        CAST(cash_limit AS DOUBLE)              AS cash_limit_mad,
-        is_cash_deposit_enabled                 AS is_cash_deposit_enabled,
-        atm_age_years                           AS atm_age_years,
-        country                                 AS country,
-        bank_name                               AS bank_name,
-        currency                                AS currency,
-        _silver_loaded_at                       AS _silver_loaded_at
+        terminal_id               AS atm_id,
+        region                    AS region,
+        atm_type                  AS atm_type,
+        provider                  AS provider,
+        location_type             AS location_type,
+        installation_date         AS installation_date,
+        CAST(cash_limit AS DOUBLE) AS cash_limit_mad,
+        is_cash_deposit_enabled   AS is_cash_deposit_enabled,
+        atm_age_years             AS atm_age_years,
+        country                   AS country,
+        bank_name                 AS bank_name,
+        currency                  AS currency,
+        _silver_loaded_at         AS _silver_loaded_at
     FROM raw
     WHERE terminal_id IS NOT NULL
 )
 
-SELECT * FROM cleaned
+SELECT *
+FROM cleaned
+QUALIFY ROW_NUMBER() OVER (PARTITION BY atm_id ORDER BY _silver_loaded_at DESC) = 1
