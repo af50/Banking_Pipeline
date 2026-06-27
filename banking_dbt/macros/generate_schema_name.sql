817504f249da3a48938d2bbca410a0ecc070e8f5
MaRiamSafwat23 <mariamsafwet23@gmail.com>
2026-06-27 13:55:12 +0300

    revert: remove cloud models

--- banking_dbt/macros/generate_schema_name.sql
+++
@@ -1,7 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if custom_schema_name is none -%}
-        {{ target.schema }}
-    {%- else -%}
-        {{ target.schema }}_{{ custom_schema_name }}
-    {%- endif -%}
-{%- endmacro %} 
